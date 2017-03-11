32771cd4af058881cdc568a259d9a12e0092d041
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-03-11 14:27:58 -0500

    Update authors, Pyro4 decorator and format methods

--- src/OpenOPC.py
+++ src/OpenOPC.py
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2007-2012 Barry Barnreiter (barry_b@users.sourceforge.net)
 # Copyright (c) 2014 Anton D. Kachalov (mouse@yandex.ru)
-# Copyright (c) 2017 by José A. Maita (jose.a.maita@gmail.com)
+# Copyright (c) 2017 José A. Maita (jose.a.maita@gmail.com)
 #
 ###########################################################################
 
